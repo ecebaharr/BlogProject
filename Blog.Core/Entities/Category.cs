12dcbb6c67d3191bcf8c7e1a4526757be64e62f2
ecebaharr <ecebahaarr@gmail.com>
2025-06-06 01:46:22 +0300

    add Post and Category entities

---
+++ Blog.Core/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }             // Primary Key
+        public string Name { get; set; }        // Kategori adı
+
+        // Her kategorinin birden fazla post'u olabilir
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ Blog.Core/Entities/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Core.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }                         // Primary Key
+        public string Title { get; set; }                   // Başlık
+        public string Content { get; set; }                 // İçerik
+        public string? ImageUrl { get; set; }               // Opsiyonel: Görsel yolu
+        public DateTime CreatedDate { get; set; } = DateTime.Now;  // Otomatik zaman
+        public bool IsPublished { get; set; } = true;       // Yayınlandı mı?
+
+        // İlişkili alanlar
+        public int CategoryId { get; set; }                 // Foreign Key
+        public Category Category { get; set; }
+    }
+}
