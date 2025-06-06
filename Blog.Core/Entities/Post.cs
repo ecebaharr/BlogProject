034951f955a6b3236ac343005ddc65b4a97358c7
ecebaharr <ecebahaarr@gmail.com>
2025-06-06 04:10:39 +0300

    moved MongoDB connection string to user-secrets and cleaned up appsettings.json

--- Blog.Core/Entities/Post.cs
+++ Blog.Core/Entities/Post.cs
@@ -1,19 +1,22 @@
-﻿using System.Text.Json.Serialization;
+﻿using Blog.Core.Entities;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
 
-namespace Blog.Core.Entities
+
+public class Post
 {
-    public class Post
-    {
-        public string? Id { get; set; }  // Mongo otomatik oluşturur
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string? ImageUrl { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public bool IsPublished { get; set; }
-        public int CategoryId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string? ImageUrl { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public bool IsPublished { get; set; }
+    public int CategoryId { get; set; }
 
-        [JsonIgnore] // Swagger'da görünmesin, istekte zorunlu olmasın
-        public Category? Category { get; set; }
-    }
+    [JsonIgnore]
+    public Category? Category { get; set; }
 }
